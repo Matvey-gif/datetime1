54acfcecf2b9c76719046c8216d9ae35536a2ccd
Matvey-git <mvityazev17120@gmail.com>
2025-11-04 22:18:33 +0500

    new commit

---
+++ 2.py
@@ -0,0 +1,10 @@
+from datetime import datetime, date, time, timedelta, timezone
+
+date1 = datetime(2024, 1, 1)
+date2 = datetime(2024, 2, 1)
+difference = date2 - date1
+
+
+if __name__ == "__main__":
+
+    print(difference.days, "день")
